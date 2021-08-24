42fa75c38aa5e92afb7f6167f4bdd619b48a5d7a
feeling404 <feeling404@outlook.com>
2021-08-24 12:09:40 +0800

    fix bip-39

--- bip39_mnemonic.go
+++ bip39_mnemonic.go
@@ -1,36 +1,29 @@
 package bls
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
-	"github.com/wxnacy/wgo/arrays"
-	"github.com/biu"
 	"io/ioutil"
+	"strconv"
 	"strings"
 
-	"strconv"
-	"bytes"
-	"crypto/rand"
-	"crypto/sha256"
-
-	"golang.org-x-text/unicode/norm"
+	"github.com/tyler-smith/go-bip39"
+	"github.com/wxnacy/wgo/arrays"
 	"golang.org/x/crypto/pbkdf2"
-	cbg "github.com/chia-bls-go"
-	"crypto/sha512"
+	"golang.org/x/text/unicode/norm"
 )
 
 func GenerateEntropy() ([]byte, error) {
-	entropy := make([]byte, 32)
-	_, err := rand.Read(entropy)
-	fmt.Println("entropy",entropy)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate entropy: %v", err)
-	}
-	return entropy, nil
+	entropy, _ := bip39.NewEntropy(256)
+	enc := entropy
+	return enc, nil
 }
 
-func getEntropyBits(entropy []byte) (string, error){
+func getEntropyBits(entropy []byte) (string, error) {
 	if 32 == len(entropy) {
-		CS := len(entropy) / 4 	//CS：8
+		CS := len(entropy) / 4 //CS：8
 
 		entropyHash := sha256.Sum256(entropy)
 		entropyHashBits := toBitsString(entropyHash[:])
@@ -44,7 +37,7 @@ func getEntropyBits(entropy []byte) (string, error){
 	return "", fmt.Errorf("required entropy length: 32 but got: %d", len(entropy))
 }
 
-func wordIndex(entropyBits string) ([]string) {
+func wordIndex(entropyBits string) []string {
 	entropyBits = strings.TrimSpace(entropyBits)
 	var sbits []string
 	index := 0
@@ -79,7 +72,7 @@ func GenerateMnemonic() ([]string, error) {
 		return nil, err
 	}
 
-	if len(entropyBits) % 11 != 0 {
+	if len(entropyBits)%11 != 0 {
 		return nil, fmt.Errorf("EntropyBits have a length mismatch")
 	}
 
@@ -89,26 +82,26 @@ func GenerateMnemonic() ([]string, error) {
 	if err != nil {
 		return nil, fmt.Errorf("can't load bip_39_word")
 	}
-	mnemonic, err := wordsFromIndex(index, wordList) 
+	mnemonic, err := wordsFromIndex(index, wordList)
 	if err != nil {
 		return nil, err
 	}
 	return mnemonic, nil
 }
 
-func CheckMnemonic(mnemonic []string) (bool) {
-	if len(mnemonic) == 24 || (arrays.ContainsString(mnemonic, "") < 0) || len(mnemonic) % 3 == 0 { 
+func CheckMnemonic(mnemonic []string) bool {
+	if len(mnemonic) == 24 || (arrays.ContainsString(mnemonic, "") < 0) || len(mnemonic)%3 == 0 {
 		//load bip39_words_list
 		wordList, err := loadWords("english.txt")
 		if err != nil {
 			return false
 		}
 		//get words index
-		var wordsIndex []int 
-		for _, word := range mnemonic{
-			index := arrays.ContainsString(wordList, word) 
+		var wordsIndex []int
+		for _, word := range mnemonic {
+			index := arrays.ContainsString(wordList, word)
 			if index < 0 {
-		 		fmt.Println(word, "is not in the mnemonic dictionary; may be misspelled")
+				fmt.Println(word, "is not in the mnemonic dictionary; may be misspelled")
 				return false
 			}
 			wordsIndex = append(wordsIndex, index)
@@ -116,13 +109,13 @@ func CheckMnemonic(mnemonic []string) (bool) {
 		//toBitString
 		var entropyBits bytes.Buffer
 		for _, index := range wordsIndex {
-			entropyBits.WriteString(fmt.Sprintf("%011b", index))	
+			entropyBits.WriteString(fmt.Sprintf("%011b", index))
 		}
 
-		CS := len(mnemonic) / 3			//8
-		ENT := len(mnemonic) * 11 - CS	//264-8=256
+		CS := len(mnemonic) / 3      //8
+		ENT := len(mnemonic)*11 - CS //264-8=256
 
-		if (len(mnemonic) * 11 == len(entropyBits.String()) && ENT % 32 == 0) {
+		if len(mnemonic)*11 == len(entropyBits.String()) && ENT%32 == 0 {
 			entropyBytes := biu.BinaryStringToBytes(entropyBits.String()[:ENT])
 			entropyBytesHash := sha256.Sum256(entropyBytes)
 			entropyBytesHashBit := toBitsString(entropyBytesHash[:])
@@ -161,7 +154,7 @@ func toBitsString(data []byte) string {
 	return buf.String()
 }
 
-func Mnemonic_to_key(mnemonic string) ([]byte){
+func Mnemonic_to_key(mnemonic string) []byte {
 	salt_str := "mnemonic" + ""
 
 	salt := norm.NFKD.String(salt_str)
@@ -169,6 +162,6 @@ func Mnemonic_to_key(mnemonic string) ([]byte){
 
 	seed := pbkdf2.Key([]byte(mnemonic_normalized), []byte(salt), 2048, 64, sha512.New)
 
-	pr_key := cbg.KeyGen(seed)
+	pr_key := KeyGen(seed)
 	return pr_key.Bytes()
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.16
 
 require (
 	github.com/kilic/bls12-381 v0.1.0
+	github.com/tyler-smith/go-bip39 v1.1.0
+	github.com/wxnacy/wgo v1.0.4
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+	golang.org/x/text v0.3.3
 )
